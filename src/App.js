0eb7106c06ba97c46d929971e0643050cd6ffb5c
Shabbir Saiyad <shabbirsaiyad83@gmail.com>
2023-09-18 23:52:56 +0530

    signup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Signup from './components/Signup';
 
 function App() {
   return (
     <div className="App">
-      <h1>E-waste</h1>
+      <Signup/>
     </div>
   );
 }
---
+++ src/components/Signup.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+     
+     <div>
+      <h1>Sign Up</h1>
+      <label htmlFor='fullname'>Fullname</label><br/>
+      <input type="text" id='fullname'></input>
+     
+    </div>
+     
+    </div>
+  )
+}
+
+export default Signup
