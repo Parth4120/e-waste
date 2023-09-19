e382379293f5fa60794f5cbc2d5ae268145671de
Shabbir Saiyad <shabbirsaiyad83@gmail.com>
2023-09-19 10:58:52 +0530

    ṣignup complete

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -7,46 +7,56 @@ import './signup.css';
 
 const Signup = () => {
   return (
+   <>
+   <div className='signupWrapper'>
     <div className='mainSignup'>
      {/* leftside */}
-     <div >
+     <div className='infoleft'>
       <h1>Sign Up</h1>
-      <label htmlFor='fullname'>Fullname</label><br/>
-      <input type="text" id='fullname'></input><br/>
-      <label htmlFor='mobile'>Mobile Number</label><br/>
-      <input type="text" id='mobile'></input><br/>
-      <label htmlFor='email'>Email Address</label><br/>
-      <input type="text" id='email'></input><br/>
-      <label htmlFor='password'>Password</label><br/>
-      <input type="text" id='password'></input>
+      <label htmlFor='fullname' className='labelContent'>Username</label>
+      <input type="text" id='fullname' className='inputbar' placeholder='Username'></input><br/>
+      <label htmlFor='mobile' className='labelContent'>Mobile Number</label>
+      <input type="text" id='mobile' placeholder='Mobile number' className='inputbar'></input><br/>
+      <label htmlFor='email' className='labelContent'>Email Address</label>
+      <input type="text" id='email' placeholder='Email-address' className='inputbar'></input><br/>
+      <label htmlFor='password' className='labelContent'>Password</label>
+      <input type="text" id='password' placeholder='Password'className='inputbar'></input>
 
     </div>
     {/* right side */}
-    <div>
+    <div className='inforight'>
         {/* cards */}
         <div className='signCard'>
              <div className='cardSignContent'>
-                <img src={producer} width='60px'></img>
+                <img src={producer} width='50px' alt='producer' className='cardimg'></img>
                 <p>Producer</p>
              </div>
              <div className='cardSignContent'>
-                <img src={manufacturer} width='60px'></img>
+                <img src={manufacturer} width='50px' alt='manufacturer' className='cardimg'></img>
                 <p>Manufacturer</p>
              </div>
              <div className='cardSignContent'>
-                <img src={recycler} width='60px'></img>
+                <img src={recycler} width='50px' alt='recycler' className='cardimg'></img>
                 <p>Recycler</p>
              </div>
              <div className='cardSignContent'>
-                <img src={refurbisher} width='60px'></img>
+                <img src={refurbisher} width='50px' alt='refurbisher' className='cardimg'></img>
                 <p>Refurbisher</p>
              </div>
         </div>
 
 
     </div>
-     
+
     </div>
+
+<div className='create-already-acount'>
+<button className='createAcountbtn'>Create Account</button>
+  <p>Already have an account ?<a href="/">Login</a></p>
+</div>
+
+</div>
+ </>
   )
 }
 
