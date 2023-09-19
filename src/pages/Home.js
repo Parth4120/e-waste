f01add07d1a8c68703f24424e88e08998aa73003
Tarangladumor <tarangladumor@gmail.com>
2023-09-19 14:38:35 +0530

    gif-text-box id done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import gifimg from'../images/gif-text.png';
 import './Home.css';
-import img from '../e-waste.png'
 import About from './About';
 import Services from './Services';
+import {BiRightArrowAlt} from 'react-icons/bi'
+
 const Home = () => {
   return (
     <div className='main-container'>
@@ -18,11 +20,33 @@ const Home = () => {
         </p>
       </div>
 
+     
+
       <div>
         <About/>
       </div>
       <div>
         <Services/>
+      </div>
+
+       {/* gif-text */}
+       <div className='gif-text'>
+        <div style={{width:'700px'}}>
+          <p className='gif-text-main-heading'>
+          "Turning the Tide on E-Waste: A Sustainable Solution for a Digital World"
+          </p>
+          
+          
+          <p className='gif-text-slogen'>Turn Your Old Gadgets into Green Rewards!</p>
+
+          <button className='explore-btn'>
+            <p>Explore more</p>
+            <BiRightArrowAlt/>
+          </button>
+        </div>
+        <div>
+          <img src={gifimg}/>
+        </div>
       </div>
     </div>
   )
